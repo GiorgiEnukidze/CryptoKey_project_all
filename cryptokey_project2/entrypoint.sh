e80a4fac07694899a503c8f5b0c6c06e036b19cf
GiorgiEnukidze <114056205+GiorgiEnukidze@users.noreply.github.com>
2024-08-30 15:37:50 +0200

    modif dockerfile backend

---
+++ cryptokey_project2/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Appliquer les migrations
+python manage.py migrate
+
+python manage.py makemigrations
+python manage.py migrate
+
+# Créer un superutilisateur si nécessaire
+if [ "$DJANGO_SUPERUSER_USERNAME" ] && [ "$DJANGO_SUPERUSER_PASSWORD" ] && [ "$DJANGO_SUPERUSER_EMAIL" ]; then
+  python manage.py createsuperuser --no-input || true
+fi
+
+# Lancer le serveur de développement Django
+exec "$@"
