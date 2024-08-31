66a2f60169b4b372726373bd258053a3637badc0
GiorgiEnukidze <114056205+GiorgiEnukidze@users.noreply.github.com>
2024-08-31 15:28:23 +0200

    modif dockerfile_back_1.

--- cryptokey_project2/wait_for_db.sh
+++ cryptokey_project2/wait_for_db.sh
@@ -3,10 +3,10 @@
 
 set -e
 
-host="$DB_HOST"
-user="$POSTGRES_USER"
-password="$POSTGRES_PASSWORD"
-db="$POSTGRES_DB"
+host="${POSTGRES_HOST:-db}"   # Use POSTGRES_HOST, default to 'db'
+user="${POSTGRES_USER:-postgres}"  # Default user to 'postgres' if not set
+password="${POSTGRES_PASSWORD}"
+db="${POSTGRES_DB:-postgres}"  # Default DB to 'postgres' if not set
 
 until PGPASSWORD=$password psql -h "$host" -U "$user" -d "$db" -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
